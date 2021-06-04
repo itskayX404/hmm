ae5f58f367554b093d5e95bb82591fbfe9b39d33
itskayX404 <81577355+itskayX404@users.noreply.github.com>
2021-06-04 13:11:29 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -6,12 +6,12 @@ var db = require(__path + '/database/db');
 try {
 var kuhong = db.get('ojan'); // jan diubah
 } catch (e) {
-	console.log('WELCOME TO ojan API!') // boleh diubah
+	console.log('WELCOME TO Kaylaa API!') // boleh diubah
 }
 
-var creatorList = ['ojan']; // Nama Lu Ngab
+var creatorList = ['KaylaChaan']; // Nama Lu Ngab
 var creator = creatorList[Math.floor(Math.random() * creatorList.length)]; // Ini jan diubah
-var key = 'OjanKeY' // Apikey Lu Ngab
+var key = 'KaylaChaan', 'KaylaCans' // Apikey Lu Ngab
 
 // Required Modules :
 var ffmpeg = require('fluent-ffmpeg');
